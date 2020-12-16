53d71fda35d135f0f6d0f6e04c35ae34deb05a32
Gabriel <g.scozzarro@gmail.com>
2020-12-16 15:18:30 +0100

    emoji plot

--- Whatsapp project/Data preparation and reading.R
+++ Whatsapp project/Data preparation and reading.R
@@ -91,9 +91,44 @@ mychat %>% mutate(day = date(time)) %>%
            ylab('Total number of messages') +
            xlab('User') +
            coord_flip() +
-           ggtitle('Total number of messages per user', 'Who has sent the most messages?, Frequency per season') +
+           ggtitle('Total number of messages per user', 'Who has sent the most messages?, Freq per season') +
            theme_minimal() +
            theme(legend.title = element_blank(), legend.position = 'bottom')
 
-#3.4 lenght of messages ----
-mychat %>%
+#3.4 Lenght of messages ----
+mychat %>% mutate(text_len = nchar(text)) %>%
+           group_by(author) %>%
+           summarise(avg_txt_len = mean(text_len)) %>%
+           ggplot(aes(author, avg_txt_len, fill = author)) +
+           geom_bar(stat = 'identity') +
+           xlab('Author') +
+           ylab('Average messages lenght') +
+           coord_flip() +
+           ggtitle('Average messages lenght by author') +
+           theme_minimal() +
+           theme(legend.title = element_blank(), legend.position = 'bottom')
+
+#What are the most used emojis in chat?
+# LIBRARY FOR EMOJI PNG IMAGE FETCH FROM https://abs.twimg.com
+library(ggimage) # EMOJI RANKING
+
+emojiplot<- mychat %>% 
+            unnest(c(emoji, emoji_name)) %>%
+            mutate(emoji = str_sub(emoji, end = 1)) %>%
+            mutate(emoji_name = str_remove(emoji_name, ':.*')) %>%
+            count(emoji, emoji_name) %>%
+            top_n(30, n) %>%
+            arrange(desc(n)) %>%
+            mutate(emoji_url = map_chr(emoji, ~paste0('https://abs.twimg.com/emoji/v2/72x72/',
+                                                      as.hexmode(utf8ToInt(.x)),'.png')))
+emojiplot %>% ggplot(aes(reorder(emoji_name, n), n)) +
+              geom_col(aes(fill = n), show.legend = FALSE, width = .2) +
+              geom_point(aes(color = n), show.legend = FALSE, size = 3) +
+              geom_image(aes(image = emoji_url), size = .045) +
+              ylab('Number of times emoji was used') +
+              xlab('Emoji meaning') +
+              ggtitle('Most used emoji') + 
+              coord_flip() +
+              theme_minimal() +
+              theme()
+            
